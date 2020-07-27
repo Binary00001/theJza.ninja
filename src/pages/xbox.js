7f6be9eae01153aedd29875da6de262387a91cc4
binary00001 <jordansdad253@gmail.com>
2020-07-27 02:55:57 -0700

    added dark bg to progress page

--- src/pages/xbox.js
+++ src/pages/xbox.js
@@ -1,22 +1,25 @@
 import React from "react";
 import ProgressBar from "react-bootstrap/ProgressBar";
+import { Link } from "react-router-dom";
 
 import Titles from "./games";
 
 const Progress = () => (
-  <>
+  <div className="bg-dark">
     {Titles.map((game, key) => (
       <div style={{ width: "50%" }} className="gameProgress">
-        <h3>{game.name}</h3>
-        <ProgressBar
-          max={game.maxGamerscore}
-          now={game.currentGamerscore}
-          variant="success"
-          //   label={`${game.maxGamerscore - game.currentGamerscore}%`}
-        />
+        <Link to={game.name} style={{ color: "white" }}>
+          <p>{game.name}</p>
+          <ProgressBar
+            max={game.maxGamerscore}
+            now={game.currentGamerscore}
+            variant="success"
+            //   label={`${game.maxGamerscore - game.currentGamerscore}%`}
+          />
+        </Link>
       </div>
     ))}
-  </>
+  </div>
 );
 
 export default Progress;
